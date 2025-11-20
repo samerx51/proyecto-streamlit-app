d2cd3984528802b78266bf490be1ad1c33e6044b
samerx51 <sanchezsam2005@gmail.com>
2025-11-20 20:15:48 -0300

    Create app.py

---
+++ data/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Aplicación Estadísticas PDI — Prototipo Inicial")
+
+st.write("""
+Bienvenido. Esta será la aplicación que mostrará estadísticas de la PDI
+usando Streamlit. En las próximas etapas agregaremos:
+
+✔ Carga de datos  
+✔ Filtros interactivos  
+✔ Gráficos  
+✔ Buscador de registros  
+""")
