ccc474ee95b3d610999dfba0aed10c4cb16b83e1
samerx51 <sanchezsam2005@gmail.com>
2025-11-25 08:51:57 -0300

    Update api_connection.py

--- api_connection.py
+++ api_connection.py
@@ -1,15 +1,27 @@
 import requests
 import pandas as pd
 
-def obtener_datos_api():
-    url = "https://api-secure.datos.gob.cl/api/v2/catalog/datasets/delitos-2024/records?limit=100"  # ejemplo
-    response = requests.get(url)
+API_URL = "https://datos.gob.cl/api/3/action/datastore_search"
+RESOURCE_ID = "4aa6ff22-e93f-42e8-8fc7-d5016b61a89f"
 
-    if response.status_code == 200:
-        data = response.json()
-        records = data["records"]
-        df = pd.json_normalize(records)
-        return df
-    else:
-        print("Error en la solicitud:", response.status_code)
-        return None
+def get_police_data(limit=5000):
+    params = {
+        "resource_id": RESOURCE_ID,
+        "limit": limit
+    }
+
+    response = requests.get(API_URL, params=params)
+
+    if response.status_code != 200:
+        raise Exception(f"Error API: {response.status_code}")
+
+    data = response.json()
+
+    records = data["result"]["records"]
+    
+    df = pd.DataFrame(records)
+    return df
+
+if __name__ == "__main__":
+    df = get_police_data()
+    print(df.head())
