bfeb8c5c1f5a8406af6b6c9c190be07c2ddc2842
samerx51 <sanchezsam2005@gmail.com>
2025-11-26 19:49:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ st.title("Análisis Estadísticas Policiales - Chile")
 
 df = get_data()
 
-st.write("Columnas recibidas:")
-st.write(df.columns.tolist())
-
+st.write("Datos obtenidos desde la API:")
 st.dataframe(df)
+
+# Filtro por región
+region = st.sidebar.selectbox(
+    "Selecciona región",
+    df["REGION UNIDAD PDI"].unique()
+)
+
+df_filtrado = df[df["REGION UNIDAD PDI"] == region]
+
+st.write(f"Datos filtrados para la región: {region}")
+st.dataframe(df_filtrado)
