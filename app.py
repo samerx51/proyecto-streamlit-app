ce36eed31674231719b791e71d0ef7f36cb011cc
samerx51 <sanchezsam2005@gmail.com>
2025-11-26 20:56:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+API_DATASETS = {
+    "Victimas": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=285a2c22-9301-4456-9e18-9fd8dbb1c6f2",
+    "Controles de identidad": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=69b8c48b-1d64-4296-8275-f3d2abfe1f0e",
+    "Denuncias": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=c4675051-558b-42d7-ad15-87f4bb6ee458",
+    "Delitos y faltas investigadas": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=b9bdcf46-f717-4dd0-8022-52e2ce3f4080",
+    "Personas detenidas": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=9afe42af-034f-4859-a479-c3b25eed49b9"
+}
 # app.py - Dashboard PDI (API + CSV) integrado y robusto
 import streamlit as st
 import pandas as pd
@@ -128,3 +139,5 @@ csv_bytes = df_to_csv_bytes(df_filtrado)
 st.download_button("Descargar CSV", data=csv_bytes, file_name="resultados_filtrados.csv", mime="text/csv")
 
 st.caption("Si quieres más visualizaciones (barras por región, series temporales, mapa, etc.), dime qué análisis deseas y lo agrego.")
+dataset_name = st.selectbox("Selecciona un tipo de delito", API_DATASETS.keys())
+url = API_DATASETS[dataset_name]
