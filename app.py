c77018609705d1f1248dc24308c1fa38bba42351
samerx51 <sanchezsam2005@gmail.com>
2025-11-26 21:05:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ import streamlit as st
 import pandas as pd
 import requests
 
+st.title("Estadísticas Policiales en Chile")
+
+delito_seleccionado = st.selectbox(
+    "Selecciona un tipo de delito:",
+    list(API_DATASETS.keys())
+)
+url = API_DATASETS[delito]
+
+response = requests.get(url).json()
+records = response["result"]["records"]
+
+df = pd.DataFrame(records)
+st.subheader(f"Datos sobre {delito}")
+st.dataframe(df)
+
+
 API_DATASETS = {
     "Victimas": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=285a2c22-9301-4456-9e18-9fd8dbb1c6f2",
     "Controles de identidad": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=69b8c48b-1d64-4296-8275-f3d2abfe1f0e",
@@ -9,6 +25,17 @@ API_DATASETS = {
     "Delitos y faltas investigadas": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=b9bdcf46-f717-4dd0-8022-52e2ce3f4080",
     "Personas detenidas": "https://datos.gob.cl/api/3/action/datastore_search?resource_id=9afe42af-034f-4859-a479-c3b25eed49b9"
 }
+def cargar_datos(url):
+    respuesta = requests.get(url)
+    
+    if respuesta.status_code == 200:
+        data = respuesta.json()
+        registros = data["result"]["records"]
+        return pd.DataFrame(registros)
+    else:
+        st.error("No se pudieron obtener los datos desde la API")
+        return pd.DataFrame()
+
 # app.py - Dashboard PDI (API + CSV) integrado y robusto
 import streamlit as st
 import pandas as pd
